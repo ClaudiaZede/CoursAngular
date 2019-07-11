47e3ecf08c67d790520f19944db2efe75411e5de
ClaudiaZede <claudia.zede@greta-sud-aquitaine.academy>
2019-07-11 16:14:04 +0200

    connexion ok

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ConnexionService } from '../services/connexion.service';
+import { Injectable } from '@angular/core';
+import { Utilisateur } from '../modele/utilisateur';
 
-@Component({
-  selector: 'app-connexion',
-  templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.css']
+@Injectable({
+  providedIn: 'root'
 })
-export class ConnexionComponent implements OnInit {
+export class ConnexionService {
 
-  donneesID = {
-    identifiant: '',
-    mdp: ''
-  };
-  
-  constructor(public conServ:ConnexionService) { }
+  user:Utilisateur;
+  connecte:boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.connecte = this.getConnecte();
+    this.user = {
+      identifiant:'',
+      mdp:''
+    }
+  }
+  // Paramétrer la connexion d'un utilisateur
+  envoieConne(){
+    console.log("Ma conne = ", this.user);
+    this.connecte = true;
+    sessionStorage.setItem('connecte', this.connecte.toString());
+  }
+  // Récupérer l'état de connexion d'un utilisateur
+  getConnecte():boolean{
+    return eval(sessionStorage.getItem('connecte'));
+  }
+  // Déconnecter l'utilisateur en prenant en compte la session
+  deconnecte(){
+    this.connecte = false;
+    sessionStorage.setItem('connecte', 'false');
+  }
+
+  toutLeMondePeutChanger(){
+    console.log("Mon ID = ", this.user.identifiant);
   }
 
 }
